b6f9086ebec59a26519b62436a114cd2e8779b37
KimToKi <craftminddream@gmail.com>
2025-01-23 23:07:38 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,38 @@
-import express from 'express';
+import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
+
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
+import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
 
-// Route สำหรับ Root URL
-app.get('/', (req, res) => {
-    res.send('Welcome to the OFA E-learning API!');
-});
+const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json()); // will allow us to parse req.body
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 
-const PORT = process.env.PORT || 5000;
+if (ENV_VARS.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`);
+	console.log("Server started at http://localhost:" + PORT);
+	connectDB();
 });
