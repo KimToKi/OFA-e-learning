930fe1b505f64536a4e759a55ba7a74fa2f111ed
KimToKi <craftminddream@gmail.com>
2025-01-23 22:36:05 +0700

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import authRoutes from "./routes/auth.route.js";
+
+const app = express();
+
+app.use("/api/v1/auth",authRoutes)
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${5000}`);
+});
