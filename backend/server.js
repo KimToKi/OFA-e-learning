5dea71f36ea8a04e955f63dc837a144ca62c8fd8
KimToKi <craftminddream@gmail.com>
2025-01-23 23:05:25 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,15 @@ import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
-app.use("/api/v1/auth",authRoutes)
+// Route สำหรับ Root URL
+app.get('/', (req, res) => {
+    res.send('Welcome to the OFA E-learning API!');
+});
 
-
+app.use("/api/v1/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${5000}`);
+    console.log(`Server started on http://localhost:${PORT}`);
 });
