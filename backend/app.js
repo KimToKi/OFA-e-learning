4c4ded3b8b5f927f6ba05904a10f0315ac2f12da
KimToKi <craftminddream@gmail.com>
2025-02-01 14:57:13 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,13 @@
-const progressRoutes = require('./routes/progressRoutes');
-app.use('/api/progress', progressRoutes);
+// backend/app.js
+import express from "express";
+import movieRouter from "./routes/movie.route.js";
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/movies", movieRouter);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
