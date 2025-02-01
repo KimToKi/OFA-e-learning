c8962796f116c3a7f746859276a42dea0797165c
KimToKi <craftminddream@gmail.com>
2025-02-01 23:34:31 +0700

    Create videosData.js

---
+++ frontend/src/data/videosData.js
@@ -0,0 +1,146 @@
+export const videos = [
+    {
+        id: "wdsc_QfFL2E",
+        snippet: {
+            title: "National Convention 2023",
+            description: "BoostUp The Power คุณเชวง คุณชยางกูร - คุณกัญญ์นลิน ปิยนันทวารินทร์ นักธุรกิจแอมเวย์ระดับมงกุฎทูตสองผู้สถาปนา แนวคิดสู่ความสำเร็จ อย่าถามตัวเองว่า ถ้าล้มเหลวล่ะ แต่ให้ถามว่า 'ถ้าสำเร็จล่ะ'",
+            thumbnails: {
+                high: {
+                    url: "https://i.ytimg.com/vi/wdsc_QfFL2E/hqdefault.jpg",
+                },
+            },
+            publishedAt: "2023-06-23T00:00:00Z",
+        },
+    },
+    {
+        id: "Ao0jHRFS-Is",
+        snippet: {
+            title: "Future Leader Club #1",
+            description: "“การตัดสินใจ และการตั้งเป้าหมาย” กิจกรรม BATCH 1 วันที่ 24 เมษายน 2562 บรรยายโดย คุณเชวง คุณชยางกูร - คุณกัญญ์นลิน ปิยนันทวารินทร์  นักธุรกิจแอมเวย์ระดับมงกุฎทูต",
+            thumbnails: {
+                high: {
+                    url: "https://i.ytimg.com/vi/Ao0jHRFS-Is/hqdefault.jpg",
+                },
+            },
+            publishedAt: "2019-06-13T00:00:00Z",
+        },
+    },
+    {
+        id: "u53KjQ2HCbc", // videoId จากลิงก์ YouTube
+        snippet: {
+            title: "FESTTALK Virtual: The Matter of SUCCESS",
+            description: "งานจบแต่แรงบันดาลใจยังไม่จบ พบกับไฮไลท์และข้อคิดมากมายจากแขกรับเชิญสุดพิเศษ พร้อมชมบรรยากาศสนุกสนานจากงาน FESTTALK Virtual วันที่ 26 กันยายน ที่ผ่านมา แบ่งปันประสบการณ์ โดย คุณรัฐธีร์ มังคลรังษี - คุณนิยุตรัตน์ จามพันธ์ นักธุรกิจแอมเวย์ระดับมงกุฎทูต",
+            thumbnails: {
+                high: {
+                    url: "https://i.ytimg.com/vi/u53KjQ2HCbc/hqdefault.jpg", // ภาพ thumbnail จาก videoId
+                },
+            },
+            publishedAt: "2020-10-11T00:00:00Z", // วันที่เผยแพร่ (กำหนดเอง)
+        },
+    },
+    {
+        id: "8akYALb-lOU", // videoId จากลิงก์ YouTube
+        snippet: {
+            title: "งานรับเข็มมงกุฏทูต : คุณแป๊ก คุณจอยซ์ ",
+            description: "แป๊ก พฤสณัย มหัคฆพงศ์ - จอยซ์ รมัณยา จูฑะเตมีย์ มงกุฏทูต, สิงหาคม 2556 Diamond, USA คนแรกของ South East Asia",
+            thumbnails: {
+                high: {
+                    url: "https://i.ytimg.com/vi/8akYALb-lOU/hqdefault.jpg", // ภาพ thumbnail จาก videoId
+                },
+            },
+            publishedAt: "2021-10-17T00:00:00Z", // วันที่เผยแพร่ (กำหนดเอง)
+        },
+    },
+    {
+        id: "iqfU3nrAYHc", // videoId จากลิงก์ YouTube
+        snippet: {
+            title: "นักธุรกิจแอมเวย์ ระดับตรีเพชร รัฐธีร์ - นิยุตรัตน์",
+            description: "รัฐธีร์ มังคลรังษี - นิยุตรัตน์ จามพันธ์ จุดสูงสุดของชีวิต มิใช่เป็นผู้ปกครอง หากแต่คือผู้รับใช้",
+            thumbnails: {
+                high: {
+                    url: "https://i.ytimg.com/vi/iqfU3nrAYHc/hqdefault.jpg", // ภาพ thumbnail จาก videoId
+                },
+            },
+            publishedAt: "2018-02-23T00:00:00Z", // วันที่เผยแพร่ (กำหนดเอง)
+        },
+    },
+    {
+        id: "MbKyfV4-rl0", // videoId จากลิงก์ YouTube
+        snippet: {
+            title: "พิธีมอบเข็ม ระดับเพชรคู่สองผู้สถาปนา",
+            description: "รับฟังประสบการณ์ความสำเร็จในพิธีมอบเข็มเกียรติคุณนักธุรกิจแอมเวย์ระดับเพชรคู่สองผู้สถาปนาใหม่ คุณพงศชา เบ็ญจพรกุลพงศ์ - คุณพิชญา มานะธัญญา เจ้าของแนวคิดความสำเร็จ : “ให้ความสำคัญกับสิ่งไหน สิ่งนั้นจะเติบโต” ",
+            thumbnails: {
+                high: {
+                    url: "https://i.ytimg.com/vi/MbKyfV4-rl0/hqdefault.jpg", // ภาพ thumbnail จาก videoId
+                },
+            },
+            publishedAt: "2023-10-03T00:00:00Z", // วันที่เผยแพร่ (กำหนดเอง)
+        },
+    },
+    {
+        id: "oyACCrjoG-s", // videoId จากลิงก์ YouTube
+        snippet: {
+            title: "EDC Recognition พี่หนึ่ง พี่ดื้อ",
+            description: "ชลธีร์ แก้วมณีสกุล รัตนพร วชิรบัญชร",
+            thumbnails: {
+                high: {
+                    url: "https://i.ytimg.com/vi/oyACCrjoG-s/hqdefault.jpg", // ภาพ thumbnail จาก videoId
+                },
+            },
+            publishedAt: "2023-09-23T00:00:00Z", // วันที่เผยแพร่ (กำหนดเอง)
+        },
+    },
+    {
+        id: "f4Mc02cxEQU", // videoId จากลิงก์ YouTube
+        snippet: {
+            title: "วงกลมวงแรก โดย ริช เดอโวส แอมเวย์",
+            description: "วงกลมวงแรก โดย ริช เดอโวส แอมเวย์",
+            thumbnails: {
+                high: {
+                    url: "https://i.ytimg.com/vi/f4Mc02cxEQU/hqdefault.jpg", // ภาพ thumbnail จาก videoId
+                },
+            },
+            publishedAt: "2011-07-26T00:00:00Z", // วันที่เผยแพร่ (กำหนดเอง)
+        },
+    },
+     {
+        id: "cH33impo3C0", // videoId จากลิงก์ YouTube
+        snippet: {
+            title: "50 Years Amway 8A_Rich",
+            description: "หลักพื้นฐาน ๘ ประการ จาก ท่านประธาน ริช เดอร์โวส เนื่องในโอกาส ฉลอง แอมเวย์ ๕๐ ปี",
+            thumbnails: {
+                high: {
+                    url: "https://i.ytimg.com/vi/cH33impo3C0/hqdefault.jpg", // ภาพ thumbnail จาก videoId
+                },
+            },
+            publishedAt: "2013-02-03T00:00:00Z", // วันที่เผยแพร่ (กำหนดเอง)
+        },
+    },
+    {
+        id: "p1VX-yFdSqY", // videoId จากลิงก์ YouTube
+        snippet: {
+            title: "Leadership Convention 2014",
+            description: "FCA FAA40 Leonard Kim at Amway Thailand Leadership Convention 2014",
+            thumbnails: {
+                high: {
+                    url: "https://i.ytimg.com/vi/p1VX-yFdSqY/hqdefault.jpg", // ภาพ thumbnail จาก videoId
+                },
+            },
+            publishedAt: "2015-07-22T00:00:00Z", // วันที่เผยแพร่ (กำหนดเอง)
+        },
+    },
+    {
+        id: "xhxGYOLHmQU", // videoId จากลิงก์ YouTube
+        snippet: {
+            title: "Crador 2018 in Bangkok (ภาษาไทย)",
+            description: "Speech by Mr.Foo Howe Kean - Crador 2018 in Bangkok (ภาษาไทย) Crador 40th Anniversary : The Great Phenomenon 13-14 October 2018 , Bangkok Thailand",
+            thumbnails: {
+                high: {
+                    url: "https://i.ytimg.com/vi/xhxGYOLHmQU/hqdefault.jpg", // ภาพ thumbnail จาก videoId
+                },
+            },
+            publishedAt: "2018-12-31T00:00:00Z", // วันที่เผยแพร่ (กำหนดเอง)
+        },
+    },
+  // เพิ่มข้อมูลวิดีโออื่น ๆ ที่นี่
+];
