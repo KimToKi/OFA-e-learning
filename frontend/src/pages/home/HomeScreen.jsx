ef3cea8a142d744a25748d686c205a5337c5fe1e
KimToKi <craftminddream@gmail.com>
2025-02-02 00:12:45 +0700

    Update HomeScreen.jsx

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Navbar from "../components/Navbar";
+import Navbar from '../../common/Navbar';
 import HeroSection from "../components/HeroSection";
 import VideoCard from "../components/VideoCard";
 import { videos } from "../data/videosData"; // นำเข้าข้อมูลวิดีโอ
