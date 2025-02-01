01b3bf55d6e1f926e3df619537f4c52a2e089912
KimToKi <craftminddream@gmail.com>
2025-02-01 15:17:19 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use("/api/movies", movieRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
