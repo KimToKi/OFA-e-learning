a4d3217a6df49554e883d5b4fe358cc07056c990
KimToKi <craftminddream@gmail.com>
2025-02-01 21:38:08 +0700

    Update movie.route.js

--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,19 +1,60 @@
-// backend/routes/movie.route.js
-import express from "express";
-import {
-	getMovieDetails,
-	getMoviesByCategory,
-	getMovieTrailers,
-	getSimilarMovies,
-	getTrendingMovie,
-} from "../controllers/movie.controller.js";
+import { fetchFromTMDB } from "../services/tmdb.service.js";
 
-const router = express.Router();
+export async function getTrendingMovie(req, res) {
+	try {
+		const data = await fetchFromTMDB("https://api.themoviedb.org/3/trending/movie/day?language=en-US");
+		const randomMovie = data.results[Math.floor(Math.random() * data.results?.length)];
 
-router.get("/trending", getTrendingMovie);
-router.get("/:id/trailers", getMovieTrailers);
-router.get("/:id/details", getMovieDetails);
-router.get("/:id/similar", getSimilarMovies);
-router.get("/:category", getMoviesByCategory);
+		res.json({ success: true, content: randomMovie });
+	} catch (error) {
+		res.status(500).json({ success: false, message: "Internal Server Error" });
+	}
+}
 
-export default router;
+export async function getMovieTrailers(req, res) {
+	const { id } = req.params;
+	try {
+		const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`);
+		res.json({ success: true, trailers: data.results });
+	} catch (error) {
+		if (error.message.includes("404")) {
+			return res.status(404).send(null);
+		}
+
+		res.status(500).json({ success: false, message: "Internal Server Error" });
+	}
+}
+
+export async function getMovieDetails(req, res) {
+	const { id } = req.params;
+	try {
+		const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}?language=en-US`);
+		res.status(200).json({ success: true, content: data });
+	} catch (error) {
+		if (error.message.includes("404")) {
+			return res.status(404).send(null);
+		}
+
+		res.status(500).json({ success: false, message: "Internal Server Error" });
+	}
+}
+
+export async function getSimilarMovies(req, res) {
+	const { id } = req.params;
+	try {
+		const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`);
+		res.status(200).json({ success: true, similar: data.results });
+	} catch (error) {
+		res.status(500).json({ success: false, message: "Internal Server Error" });
+	}
+}
+
+export async function getMoviesByCategory(req, res) {
+	const { category } = req.params;
+	try {
+		const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`);
+		res.status(200).json({ success: true, content: data.results });
+	} catch (error) {
+		res.status(500).json({ success: false, message: "Internal Server Error" });
+	}
+}
